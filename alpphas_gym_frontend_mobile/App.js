df9c8c55f0e4f45d2ed2128ebbec0d0b47c1c812
João Antonio Diniz Filho <84654509+DinizFH@users.noreply.github.com>
2025-12-01 00:00:01 -0300

    Update App.js

--- alpphas_gym_frontend_mobile/App.js
+++ alpphas_gym_frontend_mobile/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { LogBox } from 'react-native';
-import 'react-native-reanimated'; // ⚠️ deve ser o primeiro import SEMPRE!
+import 'react-native-reanimated'; 
 import { AuthProvider } from './app/context/AuthContext';
 import AppNavigator from './app/navigation/AppNavigator';
 
